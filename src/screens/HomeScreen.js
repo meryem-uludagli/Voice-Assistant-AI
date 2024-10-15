e9d21ccc4904dc1655bd4441ff245871a5331122
meryem-uludagli <meryemuslu2035@gmail.com>
2024-10-15 14:46:50 +0300

    added

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -5,21 +5,128 @@ import {
   Image,
   ScrollView,
   TouchableOpacity,
+  Alert,
 } from 'react-native';
-import React, {useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
 import Features from '../components/features';
 import {dummyMessages} from '../constants';
+import Voice from '@react-native-community/voice';
+import {apiCall} from '../api/openAI';
+import Tts from 'react-native-tts';
 
-export default function HomeScreen() {
-  const [messages, setMessages] = useState(dummyMessages);
+const HomeScreen = () => {
+  const [messages, setMessages] = useState([]);
   const [recording, setRecording] = useState(false);
+  const [speaking, setSpeaking] = useState(false);
+  const [result, setResult] = useState('');
+  const [loading, setLoading] = useState(false);
+  const ScrollViewRef = useRef();
+
+  const SpeechStartHandler = e => {
+    console.log('speech start hanldler');
+  };
+  const SpeechEndHandler = e => {
+    setRecording(false);
+    console.log('speech end hanldler');
+  };
+  const SpeechResultsHandler = e => {
+    console.log('voice event', e);
+    const text = e.value[0];
+    setResult(text);
+  };
+  const SpeechErrorHandler = e => {
+    console.log('speech error hanldler', e);
+  };
+
+  const startRecording = async () => {
+    setRecording(true);
+    Tts.stop();
+    try {
+      await Voice.start('en-GB');
+    } catch (error) {
+      console.log('error', error);
+    }
+  };
+
+  const stopRecording = async () => {
+    try {
+      await Voice.stop();
+      setRecording(false);
+      fetchResponse();
+    } catch (error) {
+      console.log('error', error);
+    }
+  };
+
+  const fetchResponse = () => {
+    if (result.trim().length > 0) {
+      let newMessages = [...messages];
+      newMessages.push({role: 'user', content: result.trim()});
+      setMessages([...newMessages]);
+      updateScrollView();
+      setLoading(true);
+
+      apiCall(result.trim(), newMessages).then(res => {
+        setLoading(false);
+        if (res.success) {
+          setMessages([...res.data]);
+          updateScrollView();
+          setResult('');
+          startTextToSpeech(res.data[res.data.length - 1]);
+        } else {
+          Alert.alert('Error', res.msg);
+        }
+      });
+    }
+  };
+  const startTextToSpeech = message => {
+    if (!message.content.includes('https')) {
+      setSpeaking(true);
+      Tts.speak(message.content, {
+        iosVoiceId: 'com.apple.ttsbundle.Moira-compact',
+        rate: 0.5,
+      });
+    }
+  };
+
+  const updateScrollView = () => {
+    setTimeout(() => {
+      ScrollViewRef?.current?.scrollToEnd({animated: true});
+    }, 2000);
+  };
+
   const clear = () => {
     setMessages([]);
+    Tts.stop();
   };
+  const stopSpeaking = () => {
+    Tts.stop();
+    setSpeaking(false);
+  };
+  useEffect(() => {
+    Voice.onSpeechStart = SpeechStartHandler;
+    Voice.onSpeechEnd = SpeechEndHandler;
+    Voice.onSpeechResults = SpeechResultsHandler;
+    Voice.onSpeechError = SpeechErrorHandler;
+
+    Tts.addEventListener('tts-start', event => console.log('start', event));
+    Tts.addEventListener('tts-progress', event =>
+      console.log('progress', event),
+    );
+    Tts.addEventListener('tts-finish', event => {
+      console.log('finish', event);
+      setSpeaking(false);
+    });
+    Tts.addEventListener('tts-cancel', event => console.log('cancel', event));
+
+    return () => {
+      Voice.destroy().then(Voice.removeAllListeners);
+    };
+  }, []);
 
   return (
     <View className="flex-1 bg-white">
@@ -38,41 +145,41 @@ export default function HomeScreen() {
               className="text-gray-700 font-semibold ml-1">
               Assistant
             </Text>
-
             <View
               style={{height: hp(58)}}
               className="bg-neutral-200 rounded-3xl p-4">
               <ScrollView
-                bounces={false}
+                ref={ScrollViewRef}
                 className="space-y-4"
+                bounces={false}
                 showsVerticalScrollIndicator={false}>
                 {messages.map((message, index) => {
-                  if (message.role === 'assistant') {
-                    return (
-                      <View key={index} className="flex-row justify-start">
-                        <View
-                          style={{width: wp(70), backgroundColor: '#DFFFD6'}}
-                          className="rounded-xl p-2">
-                          {message.content.includes('https') ? (
+                  if (message.role == 'assistant') {
+                    if (message.content.includes('https')) {
+                      return (
+                        <View key={index} className="flex-row justify-start">
+                          <View className="flex p-2 rounded-2xl bg-emerald-200 rounded-tl-none ">
                             <Image
                               source={{uri: message.content}}
                               className="rounded-2xl"
-                              resizeMode="contain"
-                              style={{
-                                width: wp(60),
-                                height: wp(40),
-                                borderRadius: 10,
-                              }}
+                              style={{height: wp(60), width: wp(60)}}
                             />
-                          ) : (
-                            <Text>{message.content}</Text>
-                          )}
+                          </View>
                         </View>
-                      </View>
-                    );
+                      );
+                    } else {
+                      return (
+                        <View
+                          key={index}
+                          style={{width: wp(70)}}
+                          className="bg-emerald-200 rounded-xl p-2 rounded-tl-none">
+                          <Text>{message.content}</Text>
+                        </View>
+                      );
+                    }
                   } else {
                     return (
-                      <View key={index} className="flex-row justify-end">
+                      <View key={index} className=" flex-row justify-end">
                         <View
                           style={{width: wp(70)}}
                           className="bg-white rounded-xl p-2 rounded-tr-none">
@@ -90,20 +197,25 @@ export default function HomeScreen() {
         )}
 
         <View className="flex justify-center items-center">
-          {recording ? (
-            <TouchableOpacity>
+          {loading ? (
+            <Image
+              source={require('../../assets/images/lg.gif')}
+              style={{width: hp(10), height: hp(10)}}
+            />
+          ) : recording ? (
+            <TouchableOpacity onPress={stopRecording}>
               <Image
                 className="rounded-full"
-                source={require('../../assets/images/lg.gif')}
+                source={require('../../assets/images/voice2.gif')}
                 style={{width: hp(10), height: hp(10)}}
               />
             </TouchableOpacity>
           ) : (
-            <TouchableOpacity>
+            <TouchableOpacity onPress={startRecording}>
               <Image
                 className="rounded-full"
                 source={require('../../assets/images/recording.png')}
-                style={{width: hp(10), height: hp(10)}}
+                style={{width: hp(8), height: hp(8)}}
               />
             </TouchableOpacity>
           )}
@@ -111,12 +223,21 @@ export default function HomeScreen() {
           {messages.length > 0 && (
             <TouchableOpacity
               onPress={clear}
-              className="bg-neutral-400 rounded-3xl p-2 absolute right-10">
-              <Text className="text-white font-semibold">Clear</Text>
+              className="bg-neutral-400 rounded-3xl  p-2  absolute right-10">
+              <Text className="text-white font-semibold"> Clear</Text>
+            </TouchableOpacity>
+          )}
+          {speaking && (
+            <TouchableOpacity
+              onPress={stopSpeaking}
+              className="bg-red-400 rounded-3xl  p-2  absolute left-10">
+              <Text className="text-white font-semibold"> Stop</Text>
             </TouchableOpacity>
           )}
         </View>
       </SafeAreaView>
     </View>
   );
-}
+};
+
+export default HomeScreen;
